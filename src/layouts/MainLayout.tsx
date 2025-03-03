90f6a1cb39e8865c90caa9c33f23eb419f35aa8a
LEE Seungseok <ssossoking@naver.com>
2025-03-03 17:26:34 +0900

    메인 레이아웃: 깃헙 액션 테스트 텍스트 업데이트

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -10,7 +10,7 @@ const MainLayout = () => {
             <header className="bg-white shadow-md">
                 <div className="container mx-auto px-4 py-4">
                     <div className="flex justify-between items-center">
-                        <Link to="/" className="text-2xl font-bold text-blue-600">깃헙 액션 테스트 성공 확인2</Link>
+                        <Link to="/" className="text-2xl font-bold text-blue-600">깃헙 액션 테스트 0303 0526</Link>
 
                         {/* 모바일 메뉴 버튼 */}
                         <div className="md:hidden">
